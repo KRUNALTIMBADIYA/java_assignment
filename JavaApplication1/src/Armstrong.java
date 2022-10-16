c37ac0247af85023ad6f5463422f110527fb9e94
krunal timbadiya <98510266+KRUNALTIMBADIYA@users.noreply.github.com>
2022-10-16 15:13:51 +0530

    this string is armstrong or not

---
+++ JavaApplication1/src/Armstrong.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Krunal
+ */
+public class Armstrong {
+public static void main(String[] args) {
+
+    String str = "Radar", reverseStr = "";
+    
+    int strLength = str.length();
+
+    for (int i = (strLength - 1); i >=0; --i) {
+      reverseStr = reverseStr + str.charAt(i);
+    }
+
+    if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+      System.out.println(str + " is a Palindrome String.");
+    }
+    else {
+      System.out.println(str + " is not a Palindrome String.");
+    }
+  }   
+}
